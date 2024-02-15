b56cbefd5616222f1d4794a9b8da7dde5910db01
Andrea Laura <andrea_carol00@hotmail.com>
2024-02-15 15:37:25 -0300

    gsap TO method
    first example using gsap

---
+++ 2. Tween/app.js
@@ -0,0 +1,11 @@
+//? gsap object = heart of gsap
+//? trim a single moment fo an animation
+
+//!gsap.method(element, vars obj)
+
+gsap.to(".box", {
+  y: 100, //transformX(100)
+  x: 200,
+  duration: 2,
+  repeat: 2,
+});
